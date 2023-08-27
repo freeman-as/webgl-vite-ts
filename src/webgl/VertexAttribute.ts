0195b24e3ad9aef325fc91d7c4cfd0321bcaedc6
freeman-as <freeman.by2018@gmail.com>
2023-08-28 00:13:35 +0900

    add vertex attibute options

--- src/webgl/VertexAttribute.ts
+++ src/webgl/VertexAttribute.ts
@@ -1,11 +1,12 @@
 export class VertexAttribute {
     public readonly data: number[];
-    public readonly location: number;
-    public readonly stride: number;
+    public readonly options: { [key in string]: number }
 
     constructor(data: number[], location: number, stride: number) {
         this.data = data;
-        this.location = location;
-        this.stride = stride;
+        this.options = {
+            location: location,
+            stride: stride
+        };
     }
 }
